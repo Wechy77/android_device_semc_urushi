b096dd54976ea51c94f09866f109dfb9e0fa46c5
szl.kiev <szl.kiev@gmail.com>
2013-02-06 17:12:49 +0700

    urushi: 4.2 initial bring-up
    Change-Id: Id857a0b90674455b0492cec2920ce7e6e26fa8ae

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,19 +1,26 @@
+# This variable is set first, so it can be overridden
+# by BoardConfigVendor.mk
 -include device/semc/mogami-common/BoardConfigCommon.mk
--include vendor/semc/urushi/BoardConfigVendor.mk
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/urushi/config
+
+# Kernel
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
 TARGET_KERNEL_CONFIG := pac_urushi_defconfig
-#TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-gnueabihf-linaro-4.7
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
+#TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-gnueabihf-linaro-4.7
 
 TARGET_RECOVERY_INITRC := device/semc/urushi/recovery/init.rc
 
+# Sensor
 SENSORS_COMPASS_AK8975 := true
 SENSORS_ACCEL_BMA150_INPUT := true
 SENSORS_ACCEL_BMA250_INPUT := false
 SENSORS_PROXIMITY_APDS9700 := true
 SENSORS_PRESSURE_BMP180 := false
 
+# Assert
 TARGET_OTA_ASSERT_DEVICE := ST18i,ST18a,urushi
 
 #twrp
@@ -22,3 +29,6 @@ RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
 TW_HAS_NO_RECOVERY_PARTITION := true
 TW_FLASH_FROM_STORAGE := true
+
+# inherit from the proprietary version
+-include vendor/semc/urushi/BoardConfigVendor.mk
